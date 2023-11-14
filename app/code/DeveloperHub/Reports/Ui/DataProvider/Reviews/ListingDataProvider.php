9491eeb6a2224944a837f99414e4da08bab723bd
Yahya062001 <144932248+Yahya062001@users.noreply.github.com>
2023-11-14 16:05:32 +0500

    Delete app/code/DeveloperHub/Reports/Ui/DataProvider/Reviews directory

--- app/code/DeveloperHub/Reports/Ui/DataProvider/Reviews/ListingDataProvider.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace DeveloperHub\HidePrice\Ui\DataProvider\Reviews;
-
-use Magento\Framework\View\Element\UiComponent\DataProvider\DataProvider;
-
-class ListingDataProvider extends DataProvider
-{
-}
